a98cf48df4da51c377546c68a3a45845d753c586
leolech14 <leonardo.lech@gmail.com>
2025-06-07 09:38:35 +0000

    chore: apply ruff and black auto-fixes to test_pdf_to_csv.py for clean pre-commit run

--- tests/test_pdf_to_csv.py
+++ tests/test_pdf_to_csv.py
@@ -1,8 +1,5 @@
 # import pytest  # F401: imported but unused
-from statement_refinery import pdf_to_csv
 
-# from statement_refinery.pdf_to_csv import parse_amount, classify_transaction
 
-# Dummy test to unblock ruff; will be overwritten by patch
 def test_dummy():
     assert True
