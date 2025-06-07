46ba64759ba53b9047bc14fcc815e5b88bc8449c
leolech14 <leonardo.lech@gmail.com>
2025-06-07 08:56:09 +0000

    chore: comment out unused imports to fix ruff F401 in test_pdf_to_csv.py

--- tests/test_pdf_to_csv.py
+++ tests/test_pdf_to_csv.py
@@ -1,3 +1,3 @@
-from statement_refinery.pdf_to_csv import parse_amount, classify_transaction
+# from statement_refinery.pdf_to_csv import parse_amount, classify_transaction
 
 # Dummy test to unblock ruff; will be overwritten by patch
